9d4fcdd01973f246616cbe81cbfcc5d082b75372
Vinay Mishra <vinaymishra2197@gmail.com>
2020-07-25 16:07:43 +0530

    custom view created programatically

--- app/src/main/java/com/xyz/app_testbook/adapter/PrepAdapter.kt
+++ app/src/main/java/com/xyz/app_testbook/adapter/PrepAdapter.kt
@@ -1,6 +1,10 @@
 package com.xyz.app_testbook.adapter
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.View.OnLongClickListener
@@ -11,6 +15,7 @@ import com.bumptech.glide.Glide
 import com.google.gson.Gson
 import com.xyz.app_testbook.R
 import com.xyz.app_testbook.data.remote.model.Class
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.row_live_class.view.*
 
 
@@ -45,7 +50,9 @@ class PrepAdapter :
             itemView.tv_questions.text = item.summary!!.module!!.count!!.quiz.toString()+"+ Questions"
             itemView.tv_notes.text = item.summary!!.module!!.count!!.notes.toString()+"+ Notes"
 
-            itemView.iv_course_logo.setColorFilter(R.color.colorPrimary)
+//            itemView.iv_course_logo.setColorFilter(R.color.colorPrimary)
+
+            drawPath(itemView)
 //            Glide
 //                .with(itemView.context)
 //                .load("https:"+item.courseLogo!!)
@@ -103,6 +110,79 @@ class PrepAdapter :
             return if (newList != null) newList.size else 0
         }
 
+    }
+
+
+
+
+    fun drawPath(itemView : View){
+        // draw circle on canvas and get bitmap
+        val bitmap = drawCircle(
+            radius = 300f,
+            canvasBackground = Color.parseColor("#FF77381F")
+        )
+
+        // show drawing on image view
+        itemView.iv_card_bg.setImageBitmap(bitmap)
+    }
+
+    // function to draw circle on canvas
+    fun drawCircle(
+        bitmapWidth:Int = 1500,
+        bitmapHeight:Int = 900,
+        radius:Float = 1500f/3,
+        cx:Float = bitmapWidth / 2f,
+        cy:Float = bitmapHeight / 1.25f,
+        canvasBackground:Int = Color.WHITE
+    ): Bitmap {
+        val bitmap = Bitmap.createBitmap(
+            bitmapWidth, bitmapHeight, Bitmap.Config.ARGB_8888
+        )
+
+        // canvas to draw circle
+        val canvas = Canvas(bitmap).apply {
+            drawColor(canvasBackground)
+        }
+
+        // paint to draw on canvas
+        val paint = Paint().apply {
+            //            this.color = color
+            this.color = Color.parseColor("#FFA04B2C")
+            isAntiAlias = true
+        }
+
+        val paint1 = Paint().apply {
+            this.color = Color.parseColor("#FFA84E2C")
+            isAntiAlias = true
+        }
+
+
+
+
+        canvas.drawCircle(
+            0f, // x-coordinate of the center of the circle
+            520f, // y-coordinate of the center of the circle
+            bitmapWidth/1.5f, // radius of the circle
+            paint1 // paint used to draw the circle
+        )
+
+        // draw circle on canvas
+        canvas.drawCircle(
+            bitmapWidth/1.25f, // x-coordinate of the center of the circle
+            bitmapHeight/1f, // y-coordinate of the center of the circle
+            bitmapWidth/2.4f, // radius of the circle
+            paint // paint used to draw the circle
+        )
+
+//        val rectF = RectF(50f, 20f, 100f, 80f)
+//        // draw circle on canvas
+//        canvas.drawArc(
+//            rectF,
+//            180f, 135f, true,
+//            paint // paint used to draw the circle
+//        )
+
+        return bitmap
     }
 
 
--- app/src/main/java/com/xyz/app_testbook/ui/MainActivity.kt
+++ app/src/main/java/com/xyz/app_testbook/ui/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.xyz.app_testbook.ui
 
+import android.graphics.*
 import android.os.Bundle
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.PagerSnapHelper
 import androidx.recyclerview.widget.SnapHelper
+import com.sdsmdg.harjot.vectormaster.models.PathModel
 import com.xyz.app_testbook.R
 import com.xyz.app_testbook.adapter.PrepAdapter
 import com.xyz.app_testbook.base.BaseActivity
 import com.xyz.app_testbook.viewmodel.MainActivityViewModel
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.min
 
 
 class MainActivity : BaseActivity() {
